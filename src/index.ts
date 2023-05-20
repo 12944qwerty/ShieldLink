ee544b0a379a6e54fafd187eccf116af3f73e6ca
12944qwerty <qwerty12944qwerty@gmail.com>
2023-05-19 21:16:00 -0500

    login and register html

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const app = express();
 app.get('/', (req: Request, res: Response) => {
   res.sendFile('index.html', { root: 'html' });
 });
+app.use(express.static('public'));
 
 app.listen(8000, () => {
   console.log('The application is listening on port 8000!');
